4dc51bd289dfbcb0b8da0fafce4acdc3e587dda5
Tim <tim.sigl@hotmail.de>
2022-01-27 11:34:22 +0100

    added nonce generation

--- clients/e-shop-demo/e-shop-browser-extension/extension/src/components/sign-in.component.js
+++ clients/e-shop-demo/e-shop-browser-extension/extension/src/components/sign-in.component.js
@@ -1,13 +1,23 @@
-const SignIn = () => {
+import { generateNonce } from '../services/authentication.service';
+import { useState } from 'react';
 
-    return(
-        <>
-        <input type="text" placeholder="Identity ID"></input>
-        <input type="password" placeholder="Secret key"></input>
-        <button>Sign in</button>
-        </>
-        
-    )
+const SignIn = () => {
+    const [identityId, setIdentityId] = useState()
+    const [nonce, setNonce] = useState()
+
+  const signInUser = async () => {
+    const nonce = await generateNonce(identityId);
+    setNonce(nonce)
+  };
+
+  return (
+    <>
+      <input type="text" placeholder="Identity ID"></input>
+      <input type="password" placeholder="Secret key" onChange={event => setIdentityId(event.target.value)}></input>
+      <button onClick={signInUser}>Sign in</button>
+      {nonce}
+    </>
+  );
 };
 
 export default SignIn;
--- clients/e-shop-demo/e-shop-browser-extension/extension/src/services/authentication.service.js
+++ clients/e-shop-demo/e-shop-browser-extension/extension/src/services/authentication.service.js
@@ -1,10 +1,12 @@
-export const generateNonce = async (id: any): Promise<string> => {
+import { client, setAuthHeader } from "../utils/axios-client";
+
+export const generateNonce = async (id) => {
 	const url = `${process.env.REACT_APP_E_SHOP_BACKEND_URL}/nonce/${id}`;
 	const response = await client.get(url);
 	return response.data.nonce;
 };
 
-export const authenticate = async (signedNonce: string, id: string): Promise<boolean> => {
+export const authenticate = async (signedNonce, id) => {
 	try {
 		const url = `${process.env.REACT_APP_E_SHOP_BACKEND_URL}/authenticate/${id}`;
 		const response = await client.post(url, JSON.stringify({ signedNonce }));
