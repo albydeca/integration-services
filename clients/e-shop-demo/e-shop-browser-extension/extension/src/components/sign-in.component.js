fc7d204a20280ec8580d10354c77d4672ce64ebd
Tim <tim.sigl@hotmail.de>
2022-01-27 15:07:15 +0100

    add nonce signing

--- clients/e-shop-demo/e-shop-browser-extension/extension/src/components/sign-in.component.js
+++ clients/e-shop-demo/e-shop-browser-extension/extension/src/components/sign-in.component.js
@@ -1,19 +1,20 @@
-import { generateNonce } from '../services/authentication.service';
+import { generateSignedNonce } from '../services/authentication.service';
 import { useState } from 'react';
 
 const SignIn = () => {
     const [identityId, setIdentityId] = useState()
+    const [secretKey, setSecretKey] = useState()
     const [nonce, setNonce] = useState()
 
   const signInUser = async () => {
-    const nonce = await generateNonce(identityId);
+    const nonce = await generateSignedNonce(identityId, secretKey);
     setNonce(nonce)
   };
 
   return (
     <>
-      <input type="text" placeholder="Identity ID"></input>
-      <input type="password" placeholder="Secret key" onChange={event => setIdentityId(event.target.value)}></input>
+      <input type="text" placeholder="Identity ID" onChange={event => setIdentityId(event.target.value)}></input>
+      <input type="password" placeholder="Secret key" onChange={event => setSecretKey(event.target.value)}></input>
       <button onClick={signInUser}>Sign in</button>
       {nonce}
     </>
--- clients/e-shop-demo/e-shop-browser-extension/extension/src/services/authentication.service.js
+++ clients/e-shop-demo/e-shop-browser-extension/extension/src/services/authentication.service.js
@@ -1,20 +1,40 @@
-import { client, setAuthHeader } from "../utils/axios-client";
+import { client, setAuthHeader } from '../utils/axios-client';
+import * as crypto from "crypto";
+import * as ed from "@noble/ed25519";
+import bs58 from "bs58";
 
-export const generateNonce = async (id) => {
-	const url = `${process.env.REACT_APP_E_SHOP_BACKEND_URL}/nonce/${id}`;
-	const response = await client.get(url);
-	return response.data.nonce;
+export const generateSignedNonce = async (id, secretKey) => {
+  const url = `${process.env.REACT_APP_E_SHOP_BACKEND_URL}/nonce/${id}`;
+  const response = await client.get(url);
+  const nonce = response.data.nonce;
+  const encodedKey = getHexEncodedKey(secretKey);
+  const signedNonce = await signNonce(encodedKey, nonce);
+  return signedNonce;
 };
 
-export const authenticate = async (signedNonce, id) => {
-	try {
-		const url = `${process.env.REACT_APP_E_SHOP_BACKEND_URL}/authenticate/${id}`;
-		const response = await client.post(url, JSON.stringify({ signedNonce }));
-		const jwt = response.data.jwt;
-		setAuthHeader(jwt);
-		return true;
-	} catch (error) {
-		console.log(error);
-		return false;
-	}
+// export const authenticate = async (signedNonce, id) => {
+// 	try {
+// 		const url = `${process.env.REACT_APP_E_SHOP_BACKEND_URL}/authenticate/${id}`;
+// 		const response = await client.post(url, JSON.stringify({ signedNonce }));
+// 		const jwt = response.data.jwt;
+// 		setAuthHeader(jwt);
+// 		return true;
+// 	} catch (error) {
+// 		console.log(error);
+// 		return false;
+// 	}
+// };
+
+const getHexEncodedKey = (base58Key) => {
+  return bs58.decode(base58Key).toString('hex');
+};
+
+const signNonce = async (secretKey, nonce) => {
+  if (nonce.length !== 40) {
+    console.log('nonce does not match length of 40 characters!');
+    process.exit();
+  }
+  const hash = crypto.createHash('sha256').update(nonce).digest().toString('hex');
+  const signedHash = await ed.sign(hash, secretKey);
+  return ed.Signature.fromHex(signedHash).toHex();
 };
